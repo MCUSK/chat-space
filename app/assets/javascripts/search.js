5c418723215dbb581adfb3c23bfd21d167efe34b
MCUSK <mcuskg@gmail.com>
2018-09-30 21:53:18 +0900

    グループユーザーの追加

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -5,7 +5,7 @@ var search_result =$("#user-search-result")
 function appendUsers(user) {
    var html =`<div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">${user.name}</p>
-                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="ユーザー名">追加</a>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-name="${user.name}" data-user-id="${user.id}" data-user-name="ユーザー名">追加</a>
               </div>`
     search_result.append(html);
   }
@@ -17,6 +17,15 @@ function appendNoUser(user){
     search_result.append(html);
 }
 
+function addUser(name, id) {
+   var html =`<div class="chat-group-user" clearfix">
+                <input type="hidden" name="group[user_ids][] value="${id}">
+                <p class="chat-group-user__name">${name}</p>
+                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+              </div>`
+    $("#chat-group-users").append(html);
+  }
+
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
     $.ajax({
@@ -25,14 +34,14 @@ function appendNoUser(user){
       data: { keyword: input },
       dataType: 'json'
     })
-   .done(function(users) {
-     $("#user-search-result").empty();
-     if (users.length !== 0) {
-       users.forEach(function(users){
-         appendUsers(users);
-       });
-     }
-     else {
+    .done(function(users) {
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(users){
+          appendUsers(users);
+        });
+      }
+      else {
       appendNoUser("該当するユーザーがいません");
      }
    })
@@ -40,4 +49,18 @@ function appendNoUser(user){
       alert('ユーザー検索に失敗しました');
     })
   });
-});
+
+  $(".chat-group-form__field--right").on("click", ".chat-group-user__btn--add", function(){
+    var name = $(this).attr("data-user-name");
+    var id = $(this).attr("data-user-id");
+    addUser(name, id)
+    $(this).parent().hide()
+  });
+
+  $(".chat-group-form__field--right").on("click", ".chat-group-user__btn--remove", function(){
+    $(this).parent().hide()
+  });
+})
+
+
+
